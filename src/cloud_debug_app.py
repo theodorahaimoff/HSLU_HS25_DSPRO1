61267e9e4763bb3311f2f4f2c4fe0d8f82421c98
theodorahaimoff <theodora.trasa@outlook.com>
2025-11-08 02:29:16 +0100

    debugging

--- src/cloud_debug_app.py
+++ src/cloud_debug_app.py
@@ -64,5 +64,5 @@ st.write("Chroma directory name:", store_dir)
 st.write("Chroma directory name:", COLLECTION_PATH)
 st.write("Chroma collection has value:", COLLECTION.count())
 
-embed = client.embeddings.create(model=MODEL_NAME)
-st.write("Embedding present:", bool(embed))
+embed = client.embeddings.create(model=MODEL_NAME, input="Kündigungsfrist")
+st.write("Embedding present:", embed)
