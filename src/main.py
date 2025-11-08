a82a96ae7c0000e76b0b44fa85d34eccdc5d23dd
theodorahaimoff <theodora.trasa@outlook.com>
2025-11-08 02:16:56 +0100

    debugging

--- src/main.py
+++ src/main.py
@@ -11,7 +11,7 @@ import logging
 from pathlib import Path
 import streamlit as st
 from typing import Iterable, Tuple
-from app_backend import answer_with_openai
+#from app_backend import answer_with_openai
 
 # Retrieval knobs
 TOP_K  = 5
